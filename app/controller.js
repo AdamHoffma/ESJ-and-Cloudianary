89549eda263240969dc152a2ffa4ada257070a25
Adam Hoffman <wintros@yahoo.com>
2019-11-29 00:04:37 -0700

    should be working but need to setup server, not sure if I'm supposed to use Morgan?

--- app/controller.js
+++ app/controller.js
@@ -17,7 +17,7 @@ module.exports = {
     create: function (req, res) {
         // Use Cloudinary uploader to upload to cloudinary server
         // Access files uploaded from the browser using req.files
-        cloudinary.v2.uploader.upload(req.files.images.path, function(result) {
+        cloudinary.v2.uploader.upload(req.files.images.path, {tags: req.body.tags}, function(result) {
             // Create a post model
             // by assembling data as object and passing to Model instance
         var post = new Model({
@@ -37,5 +37,46 @@ module.exports = {
             res.redirct('/')
             })
         })
+    },
+    index: function (req, res) {
+        Model.find({}, function (err, posts) {
+            if(err) res.send(err)
+            res.render(pages/index, {posts: posts})
+        })
+    },
+    destroy: function (req, res) {
+        var imageId = req.body.image_id
+        // The destroy method take the Image Id which we need to remove
+        cloudinary.v2.uploader.destroy(imageId, function (result) {
+            // We also delete the image details from our DB
+            Model.findOneAndRemove({image_id: imageId}, function (err) {
+                if(err) res.send(err)
+                res.redirect('/')
+            })
+        })
+    },
+    edit: function (req, res) {
+        Model.find({image_id: req.params.id}, function (err, posts) {
+            if(err) res.send(err)
+            // render edit form with existing post
+            res.render("pages/edit", {post: posts[0]})
+        })
+    },
+    /***Update Action Method */
+    update: function (req, res) {
+        var oldName = req.body.old_id
+        var newName = req.body.image_id
+        cloudinary.v2.uploader.rename(oldName, newName,
+            function(error, result) {
+                if (error) res.send(error)
+                Model.findOneAndUpdate({image_id: oldName},
+                    Object.assign({}, req.body, {image: result.url}),
+                    function (err) {
+                        if (err) 
+                            res.send(err)
+                            res.redirct('/')                        
+            })
+        })
     }
 }
+    
--- routes.js
+++ routes.js
@@ -3,8 +3,12 @@ var multipart = require('connect-multiparty')
 var multipartMiddleware = multipart()
 
 module.exports = function (app) {
+    app.get('/', controller.index)
     app.get('/new', controller.new)
     // Use Middleware to handle uploaded files and access
     // uploaded files using req.files
     app.post('/create', multipartMiddleware, controller.create)
+    app.post('destroy', controller.destroy)
+    app.post('/edit', controller.edit)
+    app.post('/update', controller.update)
 }
