73bb0ce95efbd46a9a5d8e08ce0dfe4869be6aa0
Adam Hoffman <wintros@yahoo.com>
2019-11-28 22:52:17 -0700

    setup post controlls in app/controller.js

--- app/controller.js
+++ app/controller.js
@@ -1,5 +1,41 @@
+var cloudinary = require('cloudinary')
+// Mongoose Model
+var Model = require('./model')
+
+// configure Cloudinary with you Cloudinary credentials
+
+cloudinary.config({
+    cloud_name: "hoffman-house",
+    api_key: "237152551455529",
+    api_secret: "ekb3sIe-3B0ALRY0j_KzSAve7aw"
+})
+
 module.exports = {
     new: function (req, res) {
         res.render('pages/new')
+    },
+    create: function (req, res) {
+        // Use Cloudinary uploader to upload to cloudinary server
+        // Access files uploaded from the browser using req.files
+        cloudinary.uploader.upload(req.files.images.path, function(result) {
+            // Create a post model
+            // by assembling data as object and passing to Model instance
+        var post = new Model({
+            title: req.body.title,
+            description: req.body.description,
+            created_at: new Date(),
+            // store the url in a DB for future use
+            image: result.url,
+            image_id: result.public_id
+        })
+        // persist by saving
+        post.save(function (err) {
+            if(err){
+                res.send(err)
+            }
+            // Redirect
+            res.redirct('/')
+            })
+        })
     }
 }
--- routes.js
+++ routes.js
@@ -1,5 +1,10 @@
 var controller = require("./app/controller")
+var multipart = require('connect-multiparty')
+var multipartMiddleware = multipart()
 
 module.exports = function (app) {
     app.get('/new', controller.new)
+    // Use Middleware to handle uploaded files and access
+    // uploaded files using req.files
+    app.post('/create', multipartMiddleware, controller.create)
 }
